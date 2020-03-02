a46f95336e1f3d2b6734a21edc4dcd611a73e649
Martin <hauskrechtmartin@gmail.com>
2020-03-02 22:17:18 +0100

    Add support for binary secrets

--- main.go
+++ main.go
@@ -19,7 +19,8 @@ import (
 )
 
 var (
-	region string
+	region      string
+	secretsPath string
 )
 
 type Secret struct {
@@ -30,7 +31,12 @@ type Secret struct {
 func getSecret(secretName string) *string {
 
 	//Create a Secrets Manager client
-	svc := secretsmanager.New(session.New(),
+	s, err := session.NewSession()
+	if err != nil {
+		panic(err)
+	}
+
+	svc := secretsmanager.New(s,
 		aws.NewConfig().WithRegion(region))
 	input := &secretsmanager.GetSecretValueInput{
 		SecretId:     aws.String(secretName),
@@ -46,52 +52,67 @@ func getSecret(secretName string) *string {
 			switch aerr.Code() {
 			case secretsmanager.ErrCodeDecryptionFailure:
 				// Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-				fmt.Println(secretsmanager.ErrCodeDecryptionFailure, aerr.Error())
-
+				fmt.Println(secretsmanager.ErrCodeDecryptionFailure)
+				panic(aerr.Error())
 			case secretsmanager.ErrCodeInternalServiceError:
 				// An error occurred on the server side.
 				fmt.Println(secretsmanager.ErrCodeInternalServiceError, aerr.Error())
-
+				panic(aerr.Error())
 			case secretsmanager.ErrCodeInvalidParameterException:
 				// You provided an invalid value for a parameter.
 				fmt.Println(secretsmanager.ErrCodeInvalidParameterException, aerr.Error())
-
+				panic(aerr.Error())
 			case secretsmanager.ErrCodeInvalidRequestException:
 				// You provided a parameter value that is not valid for the current state of the resource.
 				fmt.Println(secretsmanager.ErrCodeInvalidRequestException, aerr.Error())
-
+				panic(aerr.Error())
 			case secretsmanager.ErrCodeResourceNotFoundException:
 				// We can't find the resource that you asked for.
 				fmt.Println(secretsmanager.ErrCodeResourceNotFoundException, aerr.Error())
+				panic(aerr.Error())
 			}
 		} else {
 			// Print the error, cast err to awserr.Error to get the Code and
 			// Message from an error.
 			fmt.Println(err.Error())
+			panic(err.Error())
 		}
 		fmt.Println(err.Error())
-		return nil
+		panic(err.Error())
 	}
 
 	// Decrypts secret using the associated KMS CMK.
 	// Depending on whether the secret is a string or binary, one of these fields will be populated.
-	var decodedBinarySecret string
 	if result.SecretString != nil {
 		return result.SecretString
 	} else {
 		decodedBinarySecretBytes := make([]byte, base64.StdEncoding.DecodedLen(len(result.SecretBinary)))
-		len, err := base64.StdEncoding.Decode(decodedBinarySecretBytes, result.SecretBinary)
+
+		secretName := strings.Split(*result.Name, "/")
+		f, err := os.Create(secretsPath + secretName[len(secretName)-1])
+
 		if err != nil {
-			fmt.Println("Base64 Decode Error:", err)
-			return nil
+			panic(err)
 		}
-		decodedBinarySecret = string(decodedBinarySecretBytes[:len])
-		return &decodedBinarySecret
+		defer f.Close()
+
+		if _, err := f.Write(decodedBinarySecretBytes); err != nil {
+			panic(err)
+		}
+		if err := f.Sync(); err != nil {
+			panic(err)
+		}
+		return nil
 	}
 }
 
 func listAllSecrets() *secretsmanager.ListSecretsOutput {
-	svc := secretsmanager.New(session.New(),
+	s, err := session.NewSession()
+	if err != nil {
+		panic(err)
+	}
+
+	svc := secretsmanager.New(s,
 		aws.NewConfig().WithRegion(region))
 	input := &secretsmanager.ListSecretsInput{}
 
@@ -159,7 +180,13 @@ func parseSecrets(secretsNames []string) []string {
 	for _, secret := range secretsNames {
 
 		var parsedSecret map[string]string
-		err := json.Unmarshal([]byte(*getSecret(secret)), &parsedSecret)
+
+		s := getSecret(secret)
+		if s == nil {
+			continue
+		}
+
+		err := json.Unmarshal([]byte(*s), &parsedSecret)
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -175,6 +202,7 @@ func parseSecrets(secretsNames []string) []string {
 func main() {
 
 	region = os.Getenv("AWS_REGION")
+	secretsPath = os.Getenv("SM_SECRETS_PATH")
 	sm_tags := filterEnvVars("SM_TAG_")
 
 	if len(sm_tags) == 0 {
